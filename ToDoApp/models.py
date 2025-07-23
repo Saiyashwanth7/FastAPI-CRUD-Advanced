740b8a9e169a5ff1d69db040e1e69abe3cfe919b
Saiyashwanth7 <saiyashwanthdasari@gmail.com>
2025-07-23 19:18:45 +0530

    Updated Models

--- ToDoApp/models.py
+++ ToDoApp/models.py
@@ -1,6 +1,19 @@
 from database import Base
 
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+
+
+class User(Base):
+
+    __tablename__ = "usertable"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
 
 
 class Todo(Base):
@@ -12,3 +25,4 @@ class Todo(Base):
     description = Column(String)
     priority = Column(Integer)
     completed = Column(Boolean, default=False)
+    owner = Column(Integer, ForeignKey("usertable.id"))
